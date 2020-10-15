ea02378994096fe47fef59156e7990480dfcff39
alibek.nauryzbayev <alibek.nauryzbayev@qaj.kz>
2020-10-15 21:33:38 +0600

    add seeds

--- server/database/seeders/20200909033113-demo-userdata.js
+++ server/database/seeders/20200909033113-demo-userdata.js
@@ -1,10 +1,11 @@
-const faker = require("faker/locale/ru");
+const faker = require('faker/locale/ru');
 
 const users = [...Array(10).keys()].map((index) => ({
   id: faker.random.uuid(),
   fio: `${faker.name.lastName()} ${faker.name.firstName()}`,
-  login: faker.name.firstName() + index,
+  login: `admin${index}`,
   phone: faker.phone.phoneNumber(),
+  password: 'c8837b23ff8aaa8a2dde915473ce0991',
   created_at: new Date(),
   updated_at: new Date(),
 }));
@@ -22,11 +23,11 @@ const personalDatas = [...Array(10).keys()].map((index) => ({
 }));
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("users", users);
-    await queryInterface.bulkInsert("personal_data", personalDatas);
+    await queryInterface.bulkInsert('users', users);
+    await queryInterface.bulkInsert('personal_data', personalDatas);
   },
 
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete("Users", null),
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null),
   /**
    * Add commands to revert seed here.
    *
