c3271a2e9d098f06e31ee3e22d50b07ed9ca7cb5
d.kubitskiy <denis.kubicky@gmail.com>
2020-10-22 10:25:23 +0600

    Добавил в миграцию добавление городов

--- server/schemas/city.schema.js
+++ server/schemas/city.schema.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import DefaultModel from "../models/Default.model";
-
-module.exports = (sequelize, DataTypes) => {
-  class City extends DefaultModel {
-    static associate(models) {
-      models.city.belongsTo(models.geoPoint);
-    }
-  }
-  City.init(
-    {
-      id: {
-        primaryKey: true,
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4
-      },
-      name: {
-        type: DataTypes.STRING(255)
-      },
-      coordinates: {
-        type: DataTypes.GEOMETRY("POINT")
-      }
-    },
-    {
-      sequelize,
-      modelName: "city",
-      timestamps: true
-    }
-  );
-  return City;
-};
-=======
 import DefaultModel from '../models/Default.model';
 
 module.exports = (sequelize, DataTypes) => {
@@ -49,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING(255),
       },
       coordinates: {
-        type: DataTypes.GEOMETRY('POINT'),
+        type: DataTypes.GEOMETRY('MULTIPOLYGON', 4326),
       },
     },
     {
@@ -60,4 +28,3 @@ module.exports = (sequelize, DataTypes) => {
   );
   return City;
 };
->>>>>>> 5e44f60f2e8ab690c58f606ee653c7fedea786aa
